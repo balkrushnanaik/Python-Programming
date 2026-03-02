0a8e118151b6833ee43e78ddb923827bbc6e3d17
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-02 21:01:32 +0530

    first

---
+++ Control Flow and Loops/if_else.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age > 18 :
+    print("You are eligible for driving")
+elif age == 18:
+    print("Apply for driving license")
+else:
+    print("You are not eligible for driving")
+
+print("Program is done")
