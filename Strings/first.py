79e037d6c5264e7bb2fe954bf7e3583a7e27dd9e
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-06 20:14:36 +0530

    String

---
+++ Strings/first.py
@@ -0,0 +1,26 @@
+name  = "Balkrushna" #Double Quoted String
+sirName = 'Naik'
+address = ''' Karajgaon Tal- Newasa 
+Dist- Ahilyanagar
+ ''' # Triple quoted string is used for write multiline string
+
+print(name)
+print(sirName)
+print(address)
+
+#String Indexing
+
+print(name[0]) # Always start with zero
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+print(name[9])
+print(sirName[0])
+
+print(sirName[-2+3])
+
