1e5ab2a9c9f24a2b93c7269ad7790e2f4a4aafef
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-08 19:01:22 +0530

    Today I learned Modules and Pip in Python

---
+++ Functions and Modules/ModulesAndPip.py
@@ -0,0 +1,11 @@
+import math
+import myModule
+
+
+print(math.pow(2,3))
+print(math.pow(2,4))
+print(math.sqrt(100))
+print(math.sin(math.pi/2))
+
+print(myModule.who_win())
+
---
+++ Functions and Modules/myModule.py
@@ -0,0 +1,8 @@
+def greet():
+    return  "Welcome to the world of Python"
+print(greet())
+
+def who_win():
+    return "India will win Today"
+print(who_win())
+
