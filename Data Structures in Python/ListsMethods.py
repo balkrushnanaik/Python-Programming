e0852a288ce01ed39f1a441937a50cc900eab769
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-09 23:27:55 +0530

    List methods and Comprehension

---
+++ Data Structures in Python/ListsMethods.py
@@ -0,0 +1,19 @@
+marks = [10, 20, 30, 40, 50]
+print(marks)
+
+#List Methods in Python
+
+total = marks.count(10)
+print(total)
+
+marks.append(90)
+print(marks)
+
+marks.remove(20)
+print(marks)
+
+marks.reverse()
+print(marks)
+
+marks.sort()
+print(marks)
