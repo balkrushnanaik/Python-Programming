fd01edd246f4ae5d0f1b5d889355772d6a73d751
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-09 23:11:33 +0530

    Lists Concpets in Python

---
+++ Data Structures in Python/Lists.py
@@ -0,0 +1,15 @@
+#Lists are ordered, mutable(Changeable) collections of items.
+marks = [10, 20, 30, 40, 50]
+
+# We can also create mixed list
+
+mixed = [10, "Hello", True, 4.9]
+
+print(marks)
+
+print(marks[0])
+print(marks[1])
+print(marks[1:4])
+
+print(mixed)
+print(mixed[2])
