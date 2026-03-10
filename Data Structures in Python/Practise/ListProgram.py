af124a62e87ed3b74e255cd7f18557f209cf32c5
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-10 23:40:43 +0530

    List Program 1

---
+++ Data Structures in Python/Practise/ListProgram.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+print("Print the first fruit:", fruits[0])
+
+print("Replace Banana with Orange")
+fruits[1]= "Orange"
+print(fruits)
+
+print("Print length of Fruits List is :",len(fruits))
