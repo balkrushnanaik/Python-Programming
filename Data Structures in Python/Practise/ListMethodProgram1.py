38abf477c8c0084111e3dbf5897a3c2f280117a6
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-10 23:49:31 +0530

    ListMethodProgram

---
+++ Data Structures in Python/Practise/ListMethodProgram1.py
@@ -0,0 +1,15 @@
+numbers = [5, 2, 9, 1, 7]
+print(numbers)
+
+print("Sort the list in ascending order")
+numbers.sort()
+print(numbers)
+
+print("Append the number 10 to the list")
+numbers.append(10)
+print(numbers)
+
+print("Remove 2 from the list")
+numbers.remove(2)
+print(numbers)
+
