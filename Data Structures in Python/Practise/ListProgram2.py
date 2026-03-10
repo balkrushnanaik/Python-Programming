afceb7570fd4a6f4961db5ae7463d484ae175cdb
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-10 23:43:45 +0530

    Program2

---
+++ Data Structures in Python/Practise/ListProgram2.py
@@ -0,0 +1,11 @@
+num = [1,2,3,4,5,6,7,8,9,10]
+
+print(num)
+
+print("Print the first three using slicing")
+first_three = num[:3]
+print(first_three)
+
+print("Print the last three using slicing")
+last_three = num[-3:]
+print(last_three)
