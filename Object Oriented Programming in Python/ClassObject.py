021bf13d2370627f0166ff3f4ed2c76d0d0a9218
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-11 22:31:17 +0530

    Started OOPS

---
+++ Object Oriented Programming in Python/ClassObject.py
@@ -0,0 +1,16 @@
+class Employee:
+    name = "Balkrushna"
+    age = 20
+    role = "Technician"
+
+    def get_salary(self):
+        return 50000
+
+e1 = Employee()
+print(e1.name)
+print(e1.age)
+print(e1.role)
+
+print(e1.get_salary())
+
+
