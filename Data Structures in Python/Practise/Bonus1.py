a6fb9d4c02487c2d23da9556e9e814d068906962
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-11 22:59:43 +0530

    Bonus Programs

---
+++ Data Structures in Python/Practise/Bonus1.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6, 2, 3, 4, 1, 5, 7, 8, 8, 9]
+print(numbers)
+
+print("Write a program that takes a list of numbers and removes all duplicates using a set.")
+set1 = set(numbers)
+print(set1)
+
---
+++ Data Structures in Python/Practise/Bonus2.py
@@ -0,0 +1,3 @@
+products = {"Milk": 100, "Sugar": 70, "Powder":90}
+print(products)
+
