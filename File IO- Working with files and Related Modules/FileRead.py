238049d58ab2feff4fb3b96d13a89f50cf0b9c26
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-23 21:01:28 +0530

    File reading and use error handling in this code

--- File IO- Working with files and Related Modules/FileRead.py
+++ File IO- Working with files and Related Modules/FileRead.py
@@ -1,7 +1,8 @@
-f = open("sample.txt","r")
+try:
+    f = open("sample.txt","r")
 
-content = f.read()
-
-print(content)
-
-f.close() # It is very important to close the file
+    content = f.read()
+    print(content)
+    f.close()
+except FileNotFoundError:
+    print("File Not Found")
