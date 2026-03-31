e5cfe4c775ccef46688810b7dc3fcaa45cb2e2ac
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-03-31 17:56:36 +0530

    Revised Python Fundamentals

---
+++ ProgramsNew/Notes.py
@@ -0,0 +1,18 @@
+#
+# try:
+#     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#     print(type(numbers))
+#
+#     print(numbers)
+#
+#     for i in range(0, len(numbers) + 1):
+#         print(f"{i} is {numbers[i]}")
+#
+# except Exception as e:
+#     print(e)
+#
+#
+#
+
+print("C:\\Users\\Name")
+print("Hello\tWorld")
