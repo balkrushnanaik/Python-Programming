b1b4c53cf41ff1e9a47332a19909b4b84f0f3da6
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-06 22:28:03 +0530

    File Handling Example

---
+++ ProgramsNew/FileHandling.py
@@ -0,0 +1,12 @@
+# Write content to file
+
+file = open("sample.txt", "w")
+
+file.write("Hello Balkrushna!\n")
+file.write("This is file handling in Python.")
+
+file.close()
+
+print("Data written successfully.")
+
+
