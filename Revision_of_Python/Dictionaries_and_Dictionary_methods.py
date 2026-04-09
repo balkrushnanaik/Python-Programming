5f7779d3e2403427df7b3590f2863a4104300914
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-09 23:02:25 +0530

    Prime Number Program

---
+++ Revision_of_Python/Dictionaries_and_Dictionary_methods.py
@@ -0,0 +1,9 @@
+student_details = {
+    1:{"Name":"Kalyani","Age":18,"Birthday":2009},
+    2:{"Name":"Munisha","Age":19,"Birthday":2008},
+}
+
+print(student_details)
+print(student_details.keys())
+print(student_details.values())
+print(student_details.items())
