5cf5fa9f375210fedf73aa4967d6c9c9e05ed8f4
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-11 09:03:22 +0530

    Iterables & How Python Loops Over Data

---
+++ Advanced Python Concepts/Iterables_in_python.py
@@ -0,0 +1,22 @@
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for num in numbers:
+    print(num)
+
+# num1 = 10 # 'int' object is not iterable
+# for num in num1:
+#     print(num)
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+student_details = {
+    "name": "kensi",
+    "age": 18,
+    "roll_no": 23
+}
+
+for key, value in student_details.items():
+    print(key, value)
