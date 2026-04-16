2943fb9bb052d27db14b89164621873fb24c65c1
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-16 21:21:46 +0530

    Operations on Pandas

--- Library/main.py
+++ Library/main.py
@@ -29,3 +29,17 @@ print(df[['Science','English','Computer']])
 #Using Index
 print(df.iloc[0])
 
+__doc__='''Filtering Data in Pandas DataFrame \n'''
+print(__doc__)
+
+# print(df[df[['Science','Maths','English',"Computer"]]>80])
+
+print(df[df['Name']=='Sneha'])
+# print(df[df['Name']=='Riya'])
+# print(df[df['Name']=='Rahul'])
+
+__doc__='''Data Cleaning in Pandas \n'''
+print(__doc__)
+
+print(df.isnull())
+print(df.duplicated())
