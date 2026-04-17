72e2a3133b848dffaff49c28db350c7fc89463f1
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-17 22:24:23 +0530

    Read excel Data

--- Library/main1.py
+++ Library/main1.py
@@ -1,17 +1,27 @@
-# Reading CSV & Excel Files with Pandas
-
+# # Reading CSV & Excel Files with Pandas
+#
 import pandas as pd
+#
+# try:
+#     df = pd.read_csv('data.csv',usecols=['ID','First_Name','Last_Name','Age','Gender'])
+#
+#     print(df.to_string())
+#
+#     #Select all whose experience is greater than 17
+#
+#     Experience = df[df['Experience_Years']>18]
+#     print(Experience.to_string())
+#
+#
+# except Exception as e:
+#     print(e)
+#
+# # Reading Excel Files
 
 try:
-    df = pd.read_csv('data.csv')
-
-    print(df.to_string())
-
-    #Select all whose experience is greater than 17
-
-    Experience = df[df['Experience_Years']>18]
-    print(Experience.to_string())
-
+    ExcelFile = pd.read_excel('exceldata.xlsx',skiprows=[10,11,12])
+    print(ExcelFile.to_string())
 
 except Exception as e:
     print(e)
+
