910b0a812c4ebdefafa96d5a799f7199aa0bab16
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-20 18:47:26 +0530

    Learned Writting multiple sheets to excel

--- Library/main2.py
+++ Library/main2.py
@@ -1,4 +1,5 @@
-import pandas as pd
+import pandas as pd # type: ignore
+
 
 df = pd.DataFrame({
     'Name':['Barack','Obama','Donald','Balkrushna'],
@@ -7,4 +8,26 @@ df = pd.DataFrame({
 
 })
 
-df.to_csv('Data.csv', index=False)
+# df.to_csv('Data.csv', index=False)
+# df.to_excel('Data.xlsx', index=False)
+
+# Create a dataframe for products
+
+df2 = pd.DataFrame({
+    'Product':['Laptop','Smartphone','Tablet','Headphones'],
+    'Price':[1000, 500, 300, 150],
+    'Stock':[50, 200, 150, 300]
+})
+
+print(df)
+print(df2)
+
+# Writing Multiple Sheets to Excel
+
+with pd.ExcelWriter('Products.xlsx') as writer:
+    df.to_excel(writer, sheet_name='Students', index=False)
+    df2.to_excel(writer, sheet_name='Products', index=False)
+
+
+# Appending Data to Existing Files
+
