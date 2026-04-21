3b76f3089564f005159d0b0644f748b9dd04f94d
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-21 21:11:24 +0530

    Line plot

--- Library/work_on_matplot.py
+++ Library/work_on_matplot.py
@@ -4,7 +4,7 @@ import numpy as np
 x = [1, 2, 3, 4, 5]
 y = [10, 20, 15, 25, 5]
 
-plt.plot(x,y)
+plt.plot(x,y, marker='o', linestyle='--', color='b')
 plt.title('Line Graph')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
