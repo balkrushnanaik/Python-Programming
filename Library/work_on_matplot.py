31b40ea8bd4ef8c8d0726c20184151124ccfd450
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-21 21:06:30 +0530

    Started Learning matplotlib for Data Analyst

---
+++ Library/work_on_matplot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 15, 25, 5]
+
+plt.plot(x,y)
+plt.title('Line Graph')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
