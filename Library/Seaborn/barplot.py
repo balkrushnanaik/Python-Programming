0b71d7257a2460f666eeb0fa4c807d52743d08b4
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-22 21:25:35 +0530

    Learned Seaborn Library in Python

--- Library/Seaborn/barplot.py
+++ Library/Seaborn/barplot.py
@@ -5,3 +5,12 @@ import seaborn as sns # type: ignore
 # Sample data
 df = sns.load_dataset('tips')
 print(df.head()) # Head returns the first 5 rows of the dataset
+
+# Create a bar plot
+sns.barplot(x='total_bill', y='tip', data=df)
+# Set the title and labels
+plt.title('Total Bill vs Tip by Day')
+plt.xlabel('Total Bill')
+plt.ylabel('Tip')
+# Show the plot 
+plt.show()
