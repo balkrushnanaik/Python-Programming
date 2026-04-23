3522d2584a26c018128d9b0376243f40043e2c45
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-23 22:41:52 +0530

    Number Guessing Game

--- Library/Seaborn/barplot.py
+++ Library/Seaborn/barplot.py
@@ -4,10 +4,10 @@ import seaborn as sns # type: ignore
 
 # Sample data
 df = sns.load_dataset('tips')
-print(df.head()) # Head returns the first 5 rows of the dataset
+# print(df.head()) # Head returns the first 5 rows of the dataset
 
 # Create a bar plot
-sns.barplot(x='total_bill', y='tip', data=df)
+sns.barplot(x='total_bill', y='tip', data=df, ci=None) # ci=None removes the confidence interval
 # Set the title and labels
 plt.title('Total Bill vs Tip by Day')
 plt.xlabel('Total Bill')
