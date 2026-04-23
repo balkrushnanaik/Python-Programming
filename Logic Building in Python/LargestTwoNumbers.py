efbd50fc650420d8162ba20831fadb73695ecd94
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-23 23:04:24 +0530

    Which is larger and smaller number program

---
+++ Logic Building in Python/LargestTwoNumbers.py
@@ -0,0 +1,8 @@
+
+def is_larger_than(num1, num2):
+    if num1 > num2:
+        print(f"The num1: {num1} is larger than the num2: {num2}")
+    else:
+       print(f"The num1: {num1} is less than the num2: {num2}")
+print(is_larger_than(5, 4))
+
