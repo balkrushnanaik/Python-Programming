c3a2fa8ae06ac03ccfa8c653660b601827a27bb5
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-23 22:43:16 +0530

    One folder Deleted

--- ProgramsNew/ConditionalStatement.py
+++
@@ -1,8 +0,0 @@
-age = 18
-
-if age < 18:
-    print("You are minor")
-elif age == 18:
-    print("You just became an adult")
-else:
-    print("You are an adult.")
--- ProgramsNew/Natural Numbers.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number: "))
-
-if num > 1:
-    print("Natural number")
-else:
-    print("Not a natural number")
-
-
-
