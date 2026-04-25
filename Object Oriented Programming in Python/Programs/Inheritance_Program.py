ae52fb26372105cf9d523d8e73b6220c44dc5fb5
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-04-25 23:12:17 +0530

    Multilevel

--- Object Oriented Programming in Python/Programs/Inheritance_Program.py
+++ Object Oriented Programming in Python/Programs/Inheritance_Program.py
@@ -1,25 +1,40 @@
-class Person:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+# class Person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+#
+#     def show(self):
+#         print("Name:",self.name)
+#         print("Age:",self.age)
+#
+# class Student(Person):
+#    def __init__(self,name,marks):
+#        self.name = name
+#        self.marks = marks
+#        super().__init__(name,marks)
+#
+#    def display(self):
+#        print("Name:",self.name)
+#        print("Marks:",self.marks)
+#
+# s = Student("James",100)
+# p = Person("Johny",99)
+# s.show()
+# s.display()
+#
+# p.show()
 
-    def show(self):
-        print("Name:",self.name)
-        print("Age:",self.age)
+# Multilevel Inheritance
+class Animal:
+    def breathe(self): return "Breathing"
 
-class Student(Person):
-   def __init__(self,name,marks):
-       self.name = name
-       self.marks = marks
-       super().__init__(name,marks)
+class Dog(Animal):
+    def bark(self): return "Barking"
 
-   def display(self):
-       print("Name:",self.name)
-       print("Marks:",self.marks)
+class Puppy(Dog):              # Inherits from Dog AND Animal
+    def play(self): return "Playing"
 
-s = Student("James",100)
-p = Person("Johny",99)
-s.show()
-s.display()
+p = Puppy()
+print(p.breathe())             # Breathing (from Animal)
+print(p.bark())                # Barking   (from Dog)
 
-p.show()
