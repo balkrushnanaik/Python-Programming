e323c29fdd46df0a0505b6cfabc000b11d92aa3e
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-24 23:42:37 +0530

    Revised Python Fundamentals

--- Functions and Modules/defineFunction.py
+++ Functions and Modules/defineFunction.py
@@ -1,10 +1,19 @@
-def average(num1,num2,num3):
-    return num1+num2+num3
-ans = average(1,2,3)
-print(ans)
+# def average(num1,num2,num3):
+#     return num1+num2+num3
+# ans = average(1,2,3)
+# print(ans)
+#
+# def circle(Pi,radius):
+#      return Pi*radius**2
+# ans = circle(3.14,7)
+# print(ans)
+#
 
-def circle(Pi,radius):
-     return Pi*radius**2
-ans = circle(3.14,7)
-print(ans)
+def evenOdd(x):
+    if (x % 2 == 0):
+        return "Even"
+    else:
+        return "Odd"
 
+print(evenOdd(16))
+print(evenOdd(7))
