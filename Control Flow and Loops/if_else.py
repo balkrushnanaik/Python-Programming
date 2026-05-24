958d8f350e614504066f789df7a67e1c0ddc6413
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-24 23:00:47 +0530

    Revised Python Fundamentals

--- Control Flow and Loops/if_else.py
+++ Control Flow and Loops/if_else.py
@@ -1,10 +1,34 @@
 age = int(input("Enter your age: "))
 
-if age > 18 :
-    print("You are eligible for driving")
-elif age == 18:
-    print("Apply for driving license")
-else:
-    print("You are not eligible for driving")
+if age > 18 : print("You are eligible for driving")
+elif age == 18: print("Apply for driving license")
+else:  print("You are not eligible for driving")
 
-print("Program is done")
+print("------------------------------------------------------------------")
+
+age = int(input('Enter your age: '))
+if age <= 12: print("Child.")
+elif age <= 19: print("Teenager.")
+elif age <= 35: print("Young adult.")
+else: print("Adult.")
+
+
+print("-----------------------------------------------------------------------")
+age = 70
+is_member = True
+
+if age >= 60:
+    if is_member:
+        print("30% senior discount!")
+    else:
+        print("20% senior discount.")
+else:
+    print("Not eligible for a senior discount.")
+
+print('-----------------------------------------------------------------------')
+
+# Ternary Conditional Statement
+
+age = 20
+s = 'Adult' if age >= 18 else 'Teenager'
+print(s)
--- Control Flow and Loops/matchCase.py
+++ Control Flow and Loops/matchCase.py
@@ -11,3 +11,57 @@ match num:
         print("You won the New Ipad")
     case _:
         print("You lost")
+
+print('----------------------------------------------------------')
+# Grade Program using match-case
+
+# Taking grade input from user
+grade = input("Enter your grade (A/B/C/D/F): ").upper()
+
+# match checks the value of grade
+match grade:
+
+    # If grade is A
+    case 'A':
+        print("Excellent")
+
+    # If grade is B
+    case 'B':
+        print("Very Good")
+
+    # If grade is C
+    case 'C':
+        print("Good")
+
+    # If grade is D
+    case 'D':
+        print("Pass")
+
+    # If grade is F
+    case 'F':
+        print("Fail")
+
+    # Default case if no match found
+    case _:
+        print("Invalid Grade")
+
+print('---------------------------------------------------------------')
+
+marks = int(input('Enter your marks between 1 to 100: '))
+
+match marks:
+
+    case _ if marks >= 90:
+        print('Grade A')
+
+    case _ if marks >= 80:
+        print('Grade B')
+
+    case _ if marks >= 70:
+        print('Grade C')
+
+    case _ if marks >= 35:
+        print('Grade D')
+
+    case _:
+        print('Invalid Marks')
