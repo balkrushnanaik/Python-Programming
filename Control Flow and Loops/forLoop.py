4a74da591829c694b480718cef9124f5288c132a
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-24 23:24:55 +0530

    Revised Python Fundamentals

--- Control Flow and Loops/forLoop.py
+++ Control Flow and Loops/forLoop.py
@@ -1,3 +1,11 @@
-for i in range(1,11):
-    print("2 X ",i,"=",2*(i))
-    
+# For Loop:
+
+n = 11
+for i in range(1,n):
+    print(f'{5} X {i}: {i * 5}')
+
+print('------------------------------------------------')
+
+a = ["geeks", "for", "geeks"]
+for idx in range(len(a)):
+    print(a[idx])
--- Control Flow and Loops/whileLoop.py
+++ Control Flow and Loops/whileLoop.py
@@ -1,5 +1,26 @@
+# # While Loop
+# i = 2
+# while i<=10:
+#     print(" 2 X ",i,"=",2*i)
+#     i=i+1
+#
+# print('-----------------------------------------------------')
+# count = 0
+#
+# while count < 5:
+#     count += 1
+#     print("Hello World")
 
-i = 2
-while i<=10:
-    print(" 2 X ",i,"=",2*i)
-    i=i+1
+print('------------------------------------------------------------------')
+# Nested Loops
+
+# for i in range(1,5):
+#     for j in range(i):
+#         print(i, end=' ')
+#     print()
+
+for i in range(1,5):
+    for j in range(i):
+        print('*', end=' ')
+    print()
+
