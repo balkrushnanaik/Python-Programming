7e77c96b2b6597084a685887adda3c3af2b20caa
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-25 20:12:04 +0530

    Revising python data structure

---
+++ Strings/Programs/vowels_or_not.py
@@ -0,0 +1,8 @@
+char = input('Enter Any Character Between A to z: ')
+vowels = 'AEIOUaeiou'
+
+# if char in vowels: print('Vowels')
+# else: print('Consonant')
+
+if char not in vowels: print('The character is consonant')
+else: print('The character is vowels')
