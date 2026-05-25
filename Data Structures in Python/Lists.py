c80fc77d3d11738c3aa829c82f68c32cba7e7ca4
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-25 22:12:20 +0530

    Revising python data structure

--- Data Structures in Python/Lists.py
+++ Data Structures in Python/Lists.py
@@ -1,15 +1,30 @@
-#Lists are ordered, mutable(Changeable) collections of items.
-marks = [10, 20, 30, 40, 50]
+# #Lists are ordered, mutable(Changeable) collections of items.
+# marks = [10, 20, 30, 40, 50]
+#
+# # We can also create mixed list
+#
+# mixed = [10, "Hello", True, 4.9]
+#
+# print(marks)
+#
+# print(marks[0])
+# print(marks[1])
+# print(marks[1:4])
+#
+# print(mixed)
+# print(mixed[2])
 
-# We can also create mixed list
+# Using list() constructor
+num = list((1,2,3,4,5,6))
+print(type(num))
+print(num)
 
-mixed = [10, "Hello", True, 4.9]
+name = 'Balkrushna'
+names = list(('Balkrushna','Vaibhav','Dhanraj','Sid'))
+print(names)
+print(names[0])
 
-print(marks)
-
-print(marks[0])
-print(marks[1])
-print(marks[1:4])
-
-print(mixed)
-print(mixed[2])
+if name in names:
+    print('Present')
+else:
+    print('Absent')
---
+++ Strings/StringMethods.py
@@ -0,0 +1,6 @@
+s = "   ABC   "
+print(s.strip())
+
+s = "Python is fun"
+print(s.replace("fun", "awesome"))
+print(s) # Because strings in python are immutable
