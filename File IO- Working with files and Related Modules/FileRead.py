98624e07b317ac8240af3af84c3e1fca5793f9ee
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-03 21:14:20 +0530

    Revised File Handling

--- File IO- Working with files and Related Modules/FileRead.py
+++ File IO- Working with files and Related Modules/FileRead.py
@@ -1,8 +1,25 @@
-try:
-    f = open("sample.txt","r")
+# try:
+#     f = open("sample.txt","r")
+#
+#     content = f.read()
+#     print(content)
+#     f.close()
+# except FileNotFoundError:
+#     print("File Not Found")
 
-    content = f.read()
+# file = open('sample.txt','w')
+# file.write('I have stopped to watch cwh solution videos')
+# file.close()
+#
+# file1 = open('sample.txt','r')
+# content = file1.read()
+# print(content)
+# file1.close()
+
+with open('sample.txt','w') as file:
+    file.write('I have learned this new syntax')
+    file.close()
+with open('sample.txt','r') as file1:
+    content = file1.read()
     print(content)
-    f.close()
-except FileNotFoundError:
-    print("File Not Found")
+    file.close()
