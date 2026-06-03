4d9cd04e8c8aaab6bde1d3fb7d876ebab7b067be
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-03 20:57:00 +0530

    Revised Exception Handling in Pythonn

--- Advanced Python Concepts/ExceptionHandling.py
+++ Advanced Python Concepts/ExceptionHandling.py
@@ -1,16 +1,45 @@
-while True:
-    try:
-        num1 = int(input("Enter a number: "))
-        num2 = int(input("Enter another number: "))
+# while True:
+#     try:
+#         num1 = int(input("Enter a number: "))
+#         num2 = int(input("Enter another number: "))
+#
+#         print(f"The division of {num1} and {num2} is {num1 / num2}")
+#
+#     except ZeroDivisionError:
+#         print("You cannot divide by zero")
+#
+#
+#     except ValueError:
+#         print("You cannot use string in this code")
 
-        print(f"The division of {num1} and {num2} is {num1 / num2}")
+# n = 10
+# try:
+#     res = n / 0
+# except Exception as e:
+#     print(e)
+# else:
+#     print(res)
 
-    except ZeroDivisionError:
-        print("You cannot divide by zero")
+try:
+    n = 0
+    res = 100 / n
+
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+
+except ValueError:
+    print("Enter a valid number!")
+
+else:
+    print("Result is", res)
+
+finally:
+    print("Execution complete.")
 
 
-    except ValueError:
-        print("You cannot use string in this code")
+
+
+
 
 
 
