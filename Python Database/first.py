30a356ecd65a349f621a967d5a89f72100e7ecee
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-14 23:20:10 +0530

    Commit the changes

--- Python Database/first.py
+++ Python Database/first.py
@@ -1,12 +1,29 @@
+# import mysql.connector
+#
+# conn = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="Admin",
+#     database="logic_building"
+# )
+#
+# print("Connected Successfully!")
+#
+# conn.close()
 import mysql.connector
 
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Admin",
-    database="logic_building"
+    password="your_password",
+    database="company"
 )
 
-print("Connected Successfully!")
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM employees")
+
+for row in cursor.fetchall():
+    print(row)
 
 conn.close()
