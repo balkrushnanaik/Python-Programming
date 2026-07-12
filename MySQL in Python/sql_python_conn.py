d7200dfeba66123287566b7b26ee5e1d436b8861
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-12 22:17:28 +0530

    Insert data into the table

--- MySQL in Python/sql_python_conn.py
+++ MySQL in Python/sql_python_conn.py
@@ -4,7 +4,26 @@ import sqlite3
 conn = sqlite3.connect('Test_Database.sqlite')
 
 # Table Name
-conn.execute("CREATE TABLE Students(first_name TEXT, last_name TEXT, branch TEXT, marks INTEGER)")
+# conn.execute("CREATE TABLE Students(first_name TEXT, last_name TEXT, branch TEXT, marks INTEGER)")
 
 # Commit and Save
+# conn.commit()
+
+# Insert values into the table
+
+data = conn.cursor()
+
+data.execute(
+    """
+    INSERT INTO Students VALUES
+    ("Balkrushna", "Naik", "CS", 89),
+    ("Soumya", "Singh", "IT", 98),
+    ("Akash", "Tupe", "CS", 80),
+    ("Harshali", "Rahate", "CE", 97)
+    
+    
+    
+    """
+)
 conn.commit()
+
