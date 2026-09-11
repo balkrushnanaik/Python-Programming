dae9274038b9895cb753df058910eb7d4fb0f616
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-09-11 22:18:25 +0530

    Started Revising Python

--- MySQL in Python/sql_python_conn.py
+++ MySQL in Python/sql_python_conn.py
@@ -1,7 +1,8 @@
 import sqlite3
+import pandas as pd
 
 # Create a database in SQL
-conn = sqlite3.connect('Test_Database.sqlite')
+# conn = sqlite3.connect('Test_Database.sqlite')
 
 # Table Name
 # conn.execute("CREATE TABLE Students(first_name TEXT, last_name TEXT, branch TEXT, marks INTEGER)")
@@ -11,19 +12,29 @@ conn = sqlite3.connect('Test_Database.sqlite')
 
 # Insert values into the table
 
-data = conn.cursor()
+# data = conn.cursor()
+#
+# data.execute(
+#     """
+#     INSERT INTO Students VALUES
+#     ("Balkrushna", "Naik", "CS", 89),
+#     ("Soumya", "Singh", "IT", 98),
+#     ("Akash", "Tupe", "CS", 80),
+#     ("Harshali", "Rahate", "CE", 97)
+#     """
+# )
+# conn.commit()
 
-data.execute(
-    """
-    INSERT INTO Students VALUES
-    ("Balkrushna", "Naik", "CS", 89),
-    ("Soumya", "Singh", "IT", 98),
-    ("Akash", "Tupe", "CS", 80),
-    ("Harshali", "Rahate", "CE", 97)
-    
-    
-    
-    """
-)
-conn.commit()
+# Select data
+conn = sqlite3.connect('Test_Database.sqlite')
+
+query = """
+     SELECT * FROM Students
+"""
+
+data1 = pd.read_sql(query,conn)
+print(data1)
+
+
+
 
